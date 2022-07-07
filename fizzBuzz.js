8b1a4743ceb81bf163553a2614a8f3b1fb3bf210
Jay Slade <brandonslade2@gmail.com>
2022-07-06 21:24:46 -0700

    Fizz Buzz Challenge Complete

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,15 @@
 // Write your solution below this line:
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log('fizzbuzz')
+  } else if (i % 5 == 0) {
+      console.log('buzz')
+  } else if (i % 3 == 0) {
+      console.log('fizz')
+  } else {
+      console.log(i)
+  }
+  }
+
+  ///uh this is pretty much the same as the Advanced question in the MakingDecisions2....
